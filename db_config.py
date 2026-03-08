3cc048df00468c366acc00bd040f3f53330ca1be
shadha21 <shadhafathima333@gmail.com>
2026-03-08 17:15:56 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -3,9 +3,9 @@ import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.environ.get('MYSQLHOST','mysql.railway.internal'),
-        user=os.environ.get('MYSQLUSER','root'),
-        password=os.environ.get('MYSQLPASSWORD','sFBhoPiXyjGqBWcnuwxMJdRwGJSAyvYM'),
-        database=os.environ.get('MYSQLDATABASE','railway'),
-        port=int(os.environ.get('MYSQLPORT','3306'))
+        host=os.environ.get('MYSQLHOST'),
+        user=os.environ.get('MYSQLUSER'),
+        password=os.environ.get('MYSQLPASSWORD'),
+        database=os.environ.get('MYSQLDATABASE'),
+        port=int(os.environ.get('MYSQLPORT'))
     )
