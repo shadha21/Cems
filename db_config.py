cf3784decaba29ab900f953f4d70e224c686e027
shadha21 <shadhafathima333@gmail.com>
2026-03-08 15:35:58 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,28 +1,11 @@
-
-#import mysql.connector
-
-#def get_db_connection():
-#    return mysql.connector.connect(
-#        host="localhost",
-#        user="root",
-#        password="Shadha@123",
-#  )
-
 import mysql.connector
 import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("mysql.railway.internal"),
-        user=os.getenv("root"),
-        password=os.getenv("fwhOsncSkLuKMyxkQzaYuzkYaHlWsjZm"),
-        database=os.getenv("railway"),
-        port=os.getenv("3306")
-#import mysql.connector
-
-#def get_db_connection():
-#    return mysql.connector.connect(
-#        host="localhost",
-#        user="root",
-#        password="Shadha@123",
-#  )
+        host=os.environ.get("MYSQLHOST"),
+        user=os.environ.get("MYSQLUSER"),
+        password=os.environ.get("MYSQLPASSWORD"),
+        database=os.environ.get("MYSQLDATABASE"),
+        port=int(os.environ.get("MYSQLPORT"))
+    )
